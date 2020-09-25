9d084c298a528a000ab83718b5a1ba0cb72edb54
Ling Z <771152260@qq.com>
2020-09-25 10:51:56 +0800

    vuepress: deploy

--- deploy.sh
+++ deploy.sh
@@ -3,35 +3,20 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成页面文件
-if [ -d dist ]; then
-  rm -rf dist
-fi
-
-# 解决强制提交gitbook不更新的问题
-git clone -b gh-pages git@github.com:zhanglingx/blog.git dist
-cp -r assets dist
-cp -r docs/* dist
-cp -r README.md dist
-
-cd dist
-git add -A
-git commit -m 'deploy'
-git push origin gh-pages:gh-pages
-
-# mkdir dist
-# cp -r assets dist
-# cp -r docs/* dist
-# cp -r README.md dist
+# 生成静态文件
+npm run build
 
 # 进入生成的文件夹
-# cd dist
+cd docs/.vuepress/dist
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+# 发布到自定义域名
+echo 'blog.zhangling.me' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:zhanglingx/blog.git master:gh-pages
+git push -f git@github.com:zhanglingx/blog.git master:gh-pages
 
 cd -
